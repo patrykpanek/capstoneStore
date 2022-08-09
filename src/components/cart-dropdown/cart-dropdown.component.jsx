b4d0f07d61f82476d8bafc04806aab985930d278
Patryk Panek <patryyk221@gmail.com>
2022-08-09 18:17:45 +0200

    added in our addToCart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,20 @@
-import './cart-dropdown.styles.scss'
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
+
+import './cart-dropdown.styles.scss'
 
 import React from 'react'
 
 const CartDropdown = () => {
+    const { cartItems }  = useContext(CartContext);
+
   return (
     <div className='cart-dropdown-container'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+            {cartItems.map((item) => (<CartItem cartItem={item} key={item.id}/>))}
+        </div>
         <Button>CHECKOUT</Button>
     </div>
   )
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,19 @@
 import './product-card.scss';
 import Button from '../button/button.component';
 
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
+
+
 const ProductCard = ({product})=> {
     const {name, price, imageUrl} = product;
+    const {addItemToCart} = useContext(CartContext);
+
+    const addProductToCart = () =>  addItemToCart(product);
+
+
+    
     return (
         <div className='product-card-container'>
             <img  src={imageUrl} alt={name} />
@@ -10,7 +21,8 @@ const ProductCard = ({product})=> {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-        <Button buttonType='inverted'>Add to card</Button>
+            <button type='button' onClick={addProductToCart}>ADD TO CART</button>
+        {/* <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button> */}
         </div>
     )
 }
